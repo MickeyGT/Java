883d3575ec11be922c06a72cb9d0f0d23b5393cd
Turcu Gabriel <MickeyGT@users.noreply.github.com>
2017-02-24 00:32:34 +0200

    Add files via upload

---
+++ Course/Source.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+public class Source
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Hello World!");
+		ArrayList<Person> arr;
+		Person p = new Person(12,"Alex");
+		System.out.println(p);
+	}
+}
