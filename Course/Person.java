0b66fe5a8e67f0eecaa2cd0e578ef367a254c615
Turcu Gabriel <MickeyGT@users.noreply.github.com>
2017-03-01 23:20:49 +0200

    Update Person.java

--- Course/Person.java
+++ Course/Person.java
@@ -1,19 +1,43 @@
+import java.util.*;
 
 public class Person
 {
-	private int age;
-	private String name;
-	private int sed;
+    private int age;
+    private String name;
+    private int sed;
 
-	public Person(int age, String name)
-	{
-		this.age = age;
-		this.name = name;
-	}
+    public Person(int age, String name)
+    {
+        this.age = age;
+        this.name = name;
+    }
 
-	@Override
-	public String toString()
-	{
-		return "Person{"+"age="+age+", name='"+name+'\''+", sed="+sed+'}';
-	}
+    public Person oldestPerson(ArrayList<Person> vec)
+    {
+        Person oldest=vec.get(0);
+        for(Person p: vec)
+        {
+            if(p.age>oldest.age)
+                oldest=p;
+        }
+        return oldest;
+    }
+
+    public ArrayList<Person> olderThan(ArrayList<Person> vec,int givenage)
+    {
+        ArrayList<Person> temp = new ArrayList<>();
+        for(Person temporary: vec)
+        {
+            if(temporary.age>=givenage)
+                temp.add(temporary);
+        }
+        return temp;
+    }
+
+
+    @Override
+    public String toString()
+    {
+        return "Person{"+"age="+age+", name='"+name+'\''+", sed="+sed+'}';
+    }
 }
