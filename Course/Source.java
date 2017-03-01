f5db5945ffb4e8f9bf7a9b1c0764e155cc726dfc
Turcu Gabriel <MickeyGT@users.noreply.github.com>
2017-03-01 23:20:58 +0200

    Update Source.java

--- Course/Source.java
+++ Course/Source.java
@@ -1,12 +1,30 @@
-import java.util.ArrayList;
+import java.util.*;
 
 public class Source
 {
-	public static void main(String[] args)
-	{
-		System.out.println("Hello World!");
-		ArrayList<Person> arr;
-		Person p = new Person(12,"Alex");
-		System.out.println(p);
-	}
+    public static void main(String[] args)
+    {
+        /*
+        System.out.println("Welcome to the problem menu:\n");
+        System.out.println("----------------------------");
+        System.out.println("Option 1: Oldest person from given array:");
+        System.out.println("Option 2: Oldest person from given array:");
+        */
+        ArrayList<Person> arr=new ArrayList<Person>();
+        Person p = new Person(12,"Alex");
+        arr.add(p);
+        arr.add(new Person(10,"George"));
+        arr.add(new Person(34,"Ana"));
+
+        //Person reallyold;
+        ArrayList<Person>rez=new ArrayList<>();
+        //System.out.println(p);
+        //reallyold = p.oldestPerson(arr);
+        //System.out.println(reallyold);
+        rez= p.olderThan(arr,12);
+        for(Person temp:rez)
+        {
+            System.out.println(temp);
+        }
+    }
 }
