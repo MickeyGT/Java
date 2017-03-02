3e9206aba1522af118984ee9649737fd991dfa3b
Turcu Gabriel <MickeyGT@users.noreply.github.com>
2017-03-02 15:38:45 +0200

    Create Caine.java

---
+++ Course/Caine.java
@@ -0,0 +1,14 @@
+
+public class Caine extends Entity 
+{
+	public Caine(int age, String name) 
+	{
+		super(age, name);
+		
+	}
+	public String toString()
+	{
+		return "Caine {"+"age="+ age +" Absolut age="+age*9+", Name ='"+name+'\''+"}";
+		
+	}
+}
