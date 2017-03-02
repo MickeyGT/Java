02d8d03dbf22478b700f43f119da26d5775f5909
Turcu Gabriel <MickeyGT@users.noreply.github.com>
2017-03-02 15:37:58 +0200

    Create Entity.java

---
+++ Course/Entity.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+
+public abstract class Entity 
+{
+	protected int age;
+	protected String name;
+	
+	public Entity(int age, String name)
+	{
+		this.age = age;
+		this.name = name;
+	}
+}
