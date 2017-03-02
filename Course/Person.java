59a11567a1de4344ffbe8a948db078496919fe4b
Turcu Gabriel <MickeyGT@users.noreply.github.com>
2017-03-02 15:37:14 +0200

    Update Person.java

--- Course/Person.java
+++ Course/Person.java
@@ -1,43 +1,15 @@
 import java.util.*;
-
-public class Person
+public class Person extends Entity
 {
-    private int age;
-    private String name;
-    private int sed;
+	
+	public Person(int age, String name) {
+		super(age, name);
+		// TODO Auto-generated constructor stub
+	}
 
-    public Person(int age, String name)
-    {
-        this.age = age;
-        this.name = name;
-    }
-
-    public Person oldestPerson(ArrayList<Person> vec)
-    {
-        Person oldest=vec.get(0);
-        for(Person p: vec)
-        {
-            if(p.age>oldest.age)
-                oldest=p;
-        }
-        return oldest;
-    }
-
-    public ArrayList<Person> olderThan(ArrayList<Person> vec,int givenage)
-    {
-        ArrayList<Person> temp = new ArrayList<>();
-        for(Person temporary: vec)
-        {
-            if(temporary.age>=givenage)
-                temp.add(temporary);
-        }
-        return temp;
-    }
-
-
-    @Override
-    public String toString()
-    {
-        return "Person{"+"age="+age+", name='"+name+'\''+", sed="+sed+'}';
-    }
+	@Override
+	public String toString()
+	{
+		return "Person{"+"age="+ age +", name='"+name+'\''+"}";
+	}
 }
